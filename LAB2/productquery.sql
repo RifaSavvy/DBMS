3df111f4bf5b8b166d93d062c4dc9c7349aa101e
RifaSavvy <149214453+RifaSavvy@users.noreply.github.com>
2025-01-27 12:25:39 +0530

    Add files via upload

---
+++ LAB2/productquery.sql
@@ -0,0 +1,14 @@
+create table products(p_id number(10),p_price number(10),p_name char(10));
+
+insert into products values (&p_id ,&p_price,'&p_name');
+
+select min(p_price) AS Minimum_Price from products;
+
+select max(p_price) AS Maximum_Price from products;
+
+select count(p_id) AS Total_No_of_Products from products;
+
+select sum(p_price) AS Total_Price_of_all_Products from products;
+
+select avg(p_price) AS Total_Price_of_all_Products from products;
+
