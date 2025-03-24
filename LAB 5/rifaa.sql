2ebb72b2ec502a7fbb39c3095c92af503a329249
RifaSavvy <149214453+RifaSavvy@users.noreply.github.com>
2025-03-24 12:24:26 +0530

    Add files via upload

---
+++ LAB 5/rifaa.sql
@@ -0,0 +1,21 @@
+create function totalCustomers
+RETURN number IS
+	total number(2):=0;
+BEGIN
+	SELECT count
+
+
+
+
+
+
+
+
+DECLARE
+	c number(2);
+BEGIN
+	c:=totalCustomers();
+	dbms_output.put_line("Total no.of Customers: '||c);
+END;
+/
+	
